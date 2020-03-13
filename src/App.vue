058606b11a39977573aa39cfb504363b7f4e981f
paninijiahui <2957290861@qq.com>
2020-03-13 18:58:19 +0800

    'first'

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <issue></issue>
+  </div>
+</template>
+
+<script>
+import issue from './components/issue'
+
+export default {
+  name: 'App',
+  components: {
+    issue
+  }
+}
+</script>
+
+<style>
+  @import url("../src/assets/css/init.css");
+  @import url("../src/assets/css/wx_ent.css");
+  @import url("../src/assets/css/ph_issue.css");
+</style>
